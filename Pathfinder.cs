a1fb557d549721a91aa83a6d16f137fb4808ec95
Andrew Hulme <148535372+AndrewHulme88@users.noreply.github.com>
2024-11-17 20:01:12 +1100

    Add files via upload

--- Pathfinder.cs
+++ Pathfinder.cs
@@ -4,15 +4,44 @@ using UnityEngine;
 
 public class Pathfinder : MonoBehaviour
 {
-    private 
+    private EnemySpawner enemySpawner;
+    private WaveScriptableObject waveSO;
+    List<Transform> waypoints;
+    private int waypointIndex = 0;
+
+    private void Awake()
+    {
+        enemySpawner = FindObjectOfType<EnemySpawner>();
+    }
 
     void Start()
     {
-        
+        waveSO = enemySpawner.GetCurrentWave();
+        waypoints = waveSO.GetWaypoints();
+        transform.position = waypoints[waypointIndex].position;
     }
 
     void Update()
     {
         
+    }
+
+    void FollowPath()
+    {
+        if(waypointIndex < waypoints.Count)
+        {
+            Vector3 targetPosition = waypoints[waypointIndex].position;
+            float delta = waveSO.GetMoveSpeed() * Time.deltaTime;
+            transform.position = Vector2.MoveTowards(transform.position, targetPosition, delta);
+
+            if(transform.position == targetPosition )
+            {
+                waypointIndex++;
+            }
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
     }
 }
